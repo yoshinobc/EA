f636894420418415ef34fb81c88ba12ea8690093
yoshinobc <pipbcmsi@gmail.com>
2018-12-11 15:46:25 +0900

    run dynamic_function

--- function/ES/cma_es.py
+++ function/ES/cma_es.py
@@ -18,21 +18,6 @@ creator.create("Individual", list, fitness=creator.FitnessMin)
 toolbox = base.Toolbox()
 toolbox.register("evaluate", benchmarks.himmelblau)
 
-def map(gen):
-    if gen <= 22:
-        num = gen
-    elif 22 < gen <= 44:
-        num = 44 - gen
-    elif 44 < gen <= 66:
-        num = gen - 45
-    elif 66 < gen <= 88:
-        num = 88 - gen
-    elif 88 < gen:
-        num = gen - 89
-
-    return num
-
-
 def main():
     numpy.random.seed(64)
 
@@ -48,21 +33,6 @@ def main():
     centroid_array = []
     print("gen ","min ","max ","mean ","std")
     for gen in range(NGEN):
-        fig = plt.figure()
-        ax = fig.add_subplot(1,1,1)
-
-        def func(pop):
-            X = pop[0]
-            Y = pop[1]
-            return np.power((np.power(X,2) + Y - map(gen)),2) + np.power((X + np.power(Y,2) - map(gen)),2)   # 表示する計算式の指定。等高線はZに対して作られる。
-        #Z = (1 - 1/(1 + 0.05 * (np.power(X,2)+(np.power((Y - 10),2)))) - 1/(1 + 0.05*(np.power((X - 10),2) + np.power(Y,2))) - 1/(1 + 0.03*(np.power((X + 10),2) + np.power(Y,2))) - 1/(1 + 0.05*(np.power((X - 5),2) + np.power((Y + 10),2))) - 1/(1 + 0.1*(np.power((X + 5),2) + np.power((Y + 10),2))))*(1 + 0.0001*np.power((np.power(X,2) + np.power(Y,2)),1.2))
-        #plt.pcolormesh(X, Y, Z, cmap='hsv') # 等高線図の生成。cmapで色付けの規則を指定する。
-        plt.pcolormesh(X, Y, Z,cmap='hsv') # 等高線図の生成。cmapで色付けの規則を指定す
-        #pp=plt.colorbar (orientation="vertical") # カラーバーの表示
-        #pp.set_label("Label", fontname="Arial", fontsize=24) #カラーバーのラベル
-
-        plt.xlabel('X', fontsize=24)
-        plt.ylabel('Y', fontsize=24)
 
         # 新たな世代の個体群を生成
         population = toolbox.generate()
@@ -71,7 +41,7 @@ def main():
         for ind in population:
             ax.scatter(ind[0],ind[1],c='blue',marker='.')
 
-        fitnesses = toolbox.map(func, population)
+        fitnesses = toolbox.map(toolbox.evaluate, population)
         for ind, fit in zip(population, fitnesses):
             ind.fitness.values = fit
 
